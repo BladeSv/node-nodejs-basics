34ca2db0a5eec43b3685149edac4992cb08f38a2
Mikail Mitrahovich <bladesvet@gmail.com>
2022-11-28 01:13:27 +0500

    feat: add creare file implimentation

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import * as fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files", "fresh.txt");
+const content = "I am fresh and young";
+const errorMessage = "FS operation failed";
+
 const create = async () => {
-    // Write your code here 
+  fs.writeFile(filePath, content, { encoding: "utf8", flag: "wx" }).catch(
+    (err) => {
+      throw new Error(errorMessage);
+    }
+  );
 };
 
 await create();
